1032c250293bc09d958e37b45dd8179669692f61
Jarred Parr <jparr721@gmail.com>
2018-02-22 14:14:58 -0500

    CLIing almost done

--- showip.c
+++ showip.c
@@ -1,20 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <time.h>
 
-void print_usage() {
-	
+const char* print_usage() {
+	return "Usage: showip [options] hostname\noptions:\n-h Display help\n-s  Show ip and save a log of the request";
 }
 
-int main(int argc, char* argv[]) {
+void store_shown_data(char* data) {
+	printf("Storing output...");
+	time_t current_time = time(NULL);
+	FILE* data_file = fopen("data.txt", "w");
+	fclose(data_file);
+}
+
+void show_ip(char* host) {
 	struct addrinfo hints, *res, *p;
 	int status;
 	char ipstr[INET6_ADDRSTRLEN];
 
-	if (argc != 2) {
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((status = getaddrinfo(host, NULL, &hints, &res)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		exit(0);
+	}
+
+	printf("IP address for %s:\n\n", host);
+	for (p = res; p != NULL; p = p->ai_next) {
+		void *addr;
+		char *ipver;
+
+		// Get the pointer to the address
+		if (p->ai_family == AF_INET) {
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else {
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+
+	freeaddrinfo(res); // free the linked list
+}
+
+int main(int argc, char* argv[]) {
+	if (strcmp("-h", argv[1]) == 0) {
+		printf("%s", print_usage());
+		exit(1);
+	} 
+	if (strcmp("-s", argv[1]) == 0) {
+		show_ip(argv[2]);
+		exit(1);
 	}
 }
